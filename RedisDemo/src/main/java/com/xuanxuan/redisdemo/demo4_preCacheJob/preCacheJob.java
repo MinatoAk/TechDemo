5113c100277fc94b72f33143ec467dce97fb2c8b
Yukiii <2856525619@qq.com>
2024-11-12 12:04:25 +0800

    分布式锁优化定时任务做缓存预热

--- RedisDemo/src/main/java/com/xuanxuan/redisdemo/demo4_preCacheJob/preCacheJob.java
+++ RedisDemo/src/main/java/com/xuanxuan/redisdemo/demo4_preCacheJob/preCacheJob.java
@@ -1,5 +1,7 @@
 package com.xuanxuan.redisdemo.demo4_preCacheJob;
 
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 
@@ -13,20 +15,42 @@ public class preCacheJob {
     @Resource
     private RedisTemplate<String, Object> redisTemplate;
 
+    @Resource
+    private RedissonClient redissonClient;
+
     private List<Long> hotDataIds = Arrays.asList(1L, 2L, 3L);
+
+    private final String redissonKey = "scheduled:preCache:lock";
 
     /**
      * 定时做缓存预热，只缓存热点数据
      * 指定定时任务时间的语法直接网上查
      */
     @Scheduled(cron = "0 0 0 * * *")
-    public void preCacheJob() {
-        for (Long id : hotDataIds) {
-            // 1. 根据 id 拿到热点数据对应的实体对象
+    public void preCacheJob() throws InterruptedException {
+        // 1. 拿到分布式锁才能去重建缓存
+        RLock lock = redissonClient.getLock(redissonKey);
 
-            // 2. 缓存预热到数据库
-            String redisKey = String.format("system:user:%d", id);
-            redisTemplate.opsForValue().set(redisKey, "userEntity", 30, TimeUnit.MINUTES);
+        try {
+            // 2. 开看门狗机制自动续期
+            boolean success = lock.tryLock(10, -1, TimeUnit.SECONDS);
+
+            if (!success) return;
+
+            for (Long id : hotDataIds) {
+                // 1. 根据 id 拿到热点数据对应的实体对象
+
+                // 2. 缓存预热到数据库
+                String redisKey = String.format("system:user:%d", id);
+                redisTemplate.opsForValue().set(redisKey, "userEntity", 30, TimeUnit.MINUTES);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (lock != null && lock.isLocked())
+                if (lock.isHeldByCurrentThread())
+                    lock.unlock();
         }
+
     }
 }
